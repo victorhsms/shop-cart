828247d0d8c481aafa0e802f9cc6467d3971ff82
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-14 16:21:13 -0300

    Requisito 1 -supostamente- feito. Preciso fazer os testes ainda.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,11 @@
-const fetchProducts = () => {
+const fetch = require('node-fetch');
+
+const fetchProducts = (itemSearch) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${itemSearch}`;
+  return fetch(url)
+    .then((item) => item.json())
+    .then((item) => item.results);
 };
 
 if (typeof module !== 'undefined') {
