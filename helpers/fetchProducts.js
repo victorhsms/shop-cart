ca47078d487cf9cc5d834ccbc0303e72549a0a48
Victor Hugo <victorhsmsantos@gmail.com>
2022-01-14 18:33:25 -0300

    Requisitos 1 e 8 feitos

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,11 +1,9 @@
-const fetch = require('node-fetch');
-
 const fetchProducts = (itemSearch) => {
   // seu código aqui
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${itemSearch}`;
   return fetch(url)
     .then((item) => item.json())
-    .then((item) => item.results);
+    .catch(new Error('You must provide an url'));
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,7 +2,25 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
+ // Referencia do .toBeCalled e .toBeCalledWith foi retirada do README do projeto,
+ // no topico 'Pontos importantes para a implementação dos testes' que tem o link: https://jestjs.io/pt-BR/docs/expect#tohavebeencalled.
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
+    fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  });
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint esperado', () => {
+    fetchProducts('computador');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch.', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url.', async () => {
+    // Referencia do rejects: https://jestjs.io/pt-BR/docs/asynchronous#asyncawait
+    await expect(fetchProducts()).rejects.toEqual(new Error('You must provide an url'));
+  })
 });
